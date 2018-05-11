84c8e960b1199845585bed09147a8a8275a1afe1
James Russo <1jlrusso@gmail.com>
2018-05-10 17:17:17 -0700

    this file contains all of the search cases that result from user typing into the search bar

---
+++ search-cases.php
@@ -0,0 +1,62 @@
+<?php
+echo "
+  <div id='search-case-list'>
+    <a href='/displacement'>displacement</a>
+    <a href='/displacement'>initial position</a>
+    <a href='/displacement'>final position</a>
+    <a href='/displacement'>position motion</a>
+    <a href='/displacement'>negative motion</a>
+    <a href='/displacement'>distance and displacement</a>
+    <a href='/displacement'>position and time</a>
+    <a href='/displacement'>net displacement</a>
+    <a href='/2d-displacement'>vectors</a>
+    <a href='/2d-displacement'>vector displacement</a>
+    <a href='/2d-displacement'>multi-dimensional displacement</a>
+    <a href='/2d-displacement'>vector identity</a>
+    <a href='/2d-displacement'>angled displacement</a>
+    <a href='/2d-displacement'>angled travel</a>
+    <a href='/2d-displacement'>displacement trigonometry</a>
+    <a href='/2d-displacement'>inverse trigonometry</a>
+    <a href='/2d-displacement'>pythagorean theorem</a>
+    <a href='/2d-displacement'>vector addition</a>
+    <a href='/velocity'>solving velocity</a>
+    <a href='/velocity'>velocity and displacement</a>
+    <a href='/velocity'>velocity and time interval</a>
+    <a href='/velocity'>displacement and time interval</a>
+    <a href='/velocity'>speed and velocity</a>
+    <a href='/acceleration'>acceleration</a>
+    <a href='/acceleration'>changing velocity</a>
+    <a href='/acceleration'>time and acceleration</a>
+    <a href='/acceleration'>negative acceleration</a>
+    <a href='/acceleration'>positive acceleration</a>
+    <a href='/acceleration'>velocity and acceleration</a>
+    <a href='/acceleration'>time and acceleration</a>
+    <a href='/kinematics'>kinematics</a>
+    <a href='/kinematics'>big five</a>
+    <a href='/kinematics'>initial velocity kinematics</a>
+    <a href='/kinematics'>final velocity kinematics</a>
+    <a href='/kinematics'>initial position kinematics</a>
+    <a href='/kinematics'>final position kinematics</a>
+    <a href='/kinematics'>acceleration kinematics</a>
+    <a href='/kinematics'>constant acceleration</a>
+    <a href='/kinematics'>constant velocity</a>
+    <a href='/kinematics'>velocity and acceleration kinematics</a>
+    <a href='/circular-motion'>angular velocity</a>
+    <a href='/circular-motion'>period circular motion</a>
+    <a href='/circular-motion'>radius circular motion</a>
+    <a href='/circular-motion'>uniform circular motion</a>
+    <a href='/circular-motion'>frequency circular motion</a>
+    <a href='/forces'>solving net force</a>
+    <a href='/forces'>one dimensional forces</a>
+    <a href='/forces'>mass and acceleration</a>
+    <a href='/forces'>newton's laws</a>
+    <a href='/forces'>normal forces</a>
+    <a href='/gravitational-forces'>gravitational forces</a>
+    <a href='/gravitational-forces'>newton's gravitational laws</a>
+    <a href='/gravitational-forces'>inverse square law</a>
+    <a href='/gravitational-forces'>action reaction pairs</a>
+    <a href='/gravitational-forces'>gravity and acceleration</a>
+    <a href='/gravitational-forces'>gravitational constant</a>
+  </div>
+";
+?>
